774248a30b85e7ff4ae5258e24c1f94344f72ea5
mathitejeda <mathi.tejeda@gmail.com>
2020-10-31 19:11:38 -0300

    Añadida clase paquete

---
+++ Amazing/paquete.cpp
@@ -0,0 +1,1 @@
+#include "paquete.h"
---
+++ Amazing/paquete.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class paquete
+{
+public:
+	paquete();
+	~paquete();
+
+private:
+
+};
+
+paquete::paquete()
+{
+}
+
+paquete::~paquete()
+{
+}
+
