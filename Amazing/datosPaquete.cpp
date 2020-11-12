ab17b0cce8489e474b5afec45d5b98719a79c0f2
Mathias Tejeda <mathi.tejeda@gmail.com>
2020-11-11 22:57:09 -0300

    Capa de datos transporte

---
+++ Amazing/datosPaquete.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "paquete.h"
+#include "datosPaquete.h"
+
+bool datosPaquete::guardarPaquete(paquete reg)
+{
+	p = fopen(PATH_PAQUETE, "ab");
+	if (p == NULL) return false;
+	exito = fwrite(&reg, sizeof(reg), 1, p);
+	fclose(p);
+	return exito;
+}
+
+bool datosPaquete::leerPaquete(int pos)
+{
+	if (pos >= 0) {
+		p = fopen(PATH_PAQUETE, "rb");
+		if (p == NULL) return false;
+		fseek(p, pos * tamanio, SEEK_SET);
+		exito = fread(&reg, tamanio, 1, p);
+		fclose(p);
+		return exito;
+	}
+	return false;
+}
+
+bool datosPaquete::modificarPaquete(paquete mod, int pos)
+{
+	if (pos >= 0) {
+		p = fopen(PATH_PAQUETE, "rb+");
+		if (p == NULL) return false;
+		fseek(p, pos * tamanio, SEEK_SET);
+		exito = fwrite(&mod, tamanio, 1, p);
+		fclose(p);
+		return exito;
+	}
+	return false;
+}
+
+int datosPaquete::cantRegistros()
+{
+	p = fopen(PATH_PAQUETE, "rb");
+	if (p == NULL) return 0;
+	fseek(p, 0, SEEK_END);
+	int bytes = sizeof(p);
+	fclose(p);
+	return bytes / tamanio;
+}
---
+++ Amazing/datosPaquete.h
@@ -0,0 +1,16 @@
+#pragma once
+class datosPaquete
+{
+private:
+	FILE* p;
+	paquete reg;
+	int tamanio = sizeof(this->reg);
+	bool exito = false;
+	const char* PATH_PAQUETE = "../datos/paquete.dat";
+public:
+	bool guardarPaquete(paquete);
+	bool leerPaquete(int);
+	bool modificarPaquete(paquete, int);
+	int cantRegistros();
+};
+
